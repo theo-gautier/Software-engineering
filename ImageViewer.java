a1f69c6aad94614f9e20c538f2c9aba0bcbf9d74
souleymaneChs <souleymaneabdellah@gmail.com>
2017-10-17 22:44:56 +0200

    Update ImageViewer.java

--- ImageViewer.java
+++ ImageViewer.java
@@ -3,9 +3,17 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JButton;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import java.awt.Graphics2D;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFileChooser;
@@ -19,9 +27,10 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 
 	private JMenuBar menuBar = new JMenuBar();
 	private JMenu fileMenu = new JMenu("File");
-	private JMenuItem loadFile = new JMenuItem("Load"); /*  On définit le bouton charger */
+	private JMenuItem loadFile = new JMenuItem("Load"); /*  On dÃ©finit le bouton charger */
 	private JMenuItem itemClose = new JMenuItem("Close");
-	JFileChooser jfc = new JFileChooser(); /*  Affiche une fenêtre qui permet de parcourir les différents répertoires */
+	private JMenuItem itemSave = new JMenuItem("Save");	
+	JFileChooser jfc = new JFileChooser(); /*  Affiche une fenÃªtre qui permet de parcourir les diffÃ©rents rÃ©pertoires */
 
 
 	public ImageViewer () {
@@ -29,7 +38,7 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(1000, 400);
 
-		// Création d'un sélecteur de fichier.
+		// CrÃ©ation d'un sÃ©lecteur de fichier.
 		
 		
 		
@@ -61,9 +70,30 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 				System.exit(0);
 			}        
 		});
+		itemSave.addActionListener(new ActionListener() {
+               // faire une capture d'ecran de l'image a droite   
+                public void actionPerformed(ActionEvent e) {
+                BufferedImage image = new BufferedImage(output.getWidth(), output.getHeight(), BufferedImage.TYPE_INT_RGB);
+                Graphics2D g = image.createGraphics();
+                output.printAll(g);
+                g.dispose();
+                try {
+                    ImageIO.write(image, "png", new File("newImage.png"));
+                    
+                } catch (IOException exp) {
+                    exp.printStackTrace();
+                    }
+                }
+               });
+		
+		
+		
+		
+		
 		this.fileMenu.add(itemClose);  
+		this.fileMenu.add(itemSave);
 		this.fileMenu.add(loadFile); /* On ajoute au bonton File l'action le sous-bouton charger */
-		this.loadFile.addActionListener(new ActionListener(){ /* On ajoute au bouton load un détecteur de click */
+		this.loadFile.addActionListener(new ActionListener(){ /* On ajoute au bouton load un dÃ©tecteur de click */
 			public void actionPerformed(ActionEvent arg0) 
 			{
 			    if (arg0.getSource() == loadFile) {
@@ -74,9 +104,9 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 			        	inputImage.refreshImage(path);
 			        	ouputImage.refreshImage(path);
 			        	
-			    		input.revalidate(); /* On rafraîchit l'image de gauche */ 
+			    		input.revalidate(); /* On rafraÃ®chit l'image de gauche */ 
 			    		input.repaint();
-			    		output.revalidate(); /* On rafraîchit l'image de droite */
+			    		output.revalidate(); /* On rafraÃ®chit l'image de droite */
 			    		output.repaint();		
 			        }
 			    }
