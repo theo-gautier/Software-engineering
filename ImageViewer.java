090dacce402882747abf9aa8635b8b1c6c7339ae
rimesup <adel.bouraoui@depinfonancy.net>
2017-10-17 22:07:33 +0200

    Add files via upload

--- ImageViewer.java
+++ ImageViewer.java
@@ -3,39 +3,36 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-
-import org.jfree.ui.RefineryUtilities;
-
 import javax.swing.JButton;
 import javax.swing.BoxLayout;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JFileChooser;
 
 public class ImageViewer extends JFrame /*implements ActionListener*/
 {
-	
-	//Création des images qui vont être affichées.
+	private String path; 
 	private DisplayedImage inputImage = new DisplayedImage(); 
 	private DisplayedImage ouputImage = new DisplayedImage();
-	
-	//Création des deux boutons clickables.
 	private JButton buttonAction = new JButton("Action");
-	private JButton buttonInversion = new JButton("Inversion");
-	private JButton buttonBarChart = new JButton("Bar Chart");
 
-	//Création du header où se trouve un bouton File, et un sous-bouton Close.
 	private JMenuBar menuBar = new JMenuBar();
 	private JMenu fileMenu = new JMenu("File");
-
+	private JMenuItem loadFile = new JMenuItem("Load"); /*  On définit le bouton charger */
 	private JMenuItem itemClose = new JMenuItem("Close");
+	JFileChooser jfc = new JFileChooser(); /*  Affiche une fenêtre qui permet de parcourir les différents répertoires */
 
-	//On s'attache ici à la création du Canvas pour accueillir les images.
+
 	public ImageViewer () {
 		this.setTitle("Image Viewer");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(1000, 400);
 
+		// Création d'un sélecteur de fichier.
+		
+		
+		
 		JPanel input = new JPanel();
 		input.setLayout(new BoxLayout(input, BoxLayout.PAGE_AXIS));
 		input.add(inputImage);
@@ -45,17 +42,6 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 		action.add(buttonAction);
 		// Defines action associated to buttons
 		buttonAction.addActionListener(new ButtonListener());
-		
-		JPanel inversion = new JPanel();
-		inversion.setLayout(new BoxLayout(inversion, BoxLayout.PAGE_AXIS));
-		action.add(buttonInversion);
-		// Defines action associated to buttons
-		buttonInversion.addActionListener(new ButtonListener());
-		
-		JPanel barchart = new JPanel();
-		barchart.setLayout(new BoxLayout(inversion, BoxLayout.PAGE_AXIS));
-		action.add(buttonBarChart);
-		buttonBarChart.addActionListener(new ButtonBarChartListener());
 
 		JPanel output = new JPanel();
 		output.setLayout(new BoxLayout(output, BoxLayout.PAGE_AXIS));
@@ -76,31 +62,39 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 			}        
 		});
 		this.fileMenu.add(itemClose);  
-
-		this.menuBar.add(fileMenu);
+		this.fileMenu.add(loadFile); /* On ajoute au bonton File l'action le sous-bouton charger */
+		this.loadFile.addActionListener(new ActionListener(){ /* On ajoute au bouton load un détecteur de click */
+			public void actionPerformed(ActionEvent arg0) 
+			{
+			    if (arg0.getSource() == loadFile) {
+			        int returnVal = jfc.showOpenDialog(null);
+			        
+			        if (returnVal == JFileChooser.APPROVE_OPTION) {
+			        	path=jfc.getSelectedFile().getAbsolutePath();
+			        	inputImage.refreshImage(path);
+			        	ouputImage.refreshImage(path);
+			        	
+			    		input.revalidate(); /* On rafraîchit l'image de gauche */ 
+			    		input.repaint();
+			    		output.revalidate(); /* On rafraîchit l'image de droite */
+			    		output.repaint();		
+			        }
+			    }
+			}
+		});
+		this.menuBar.add(fileMenu); 
 		this.setJMenuBar(menuBar);
-
 		this.setVisible(true);
+		
 	}
 
-	/*
+	/**
 	 * Class listening to a given button
-	 * Attends qu'un click soit effectué sur un bouton pour afficher le message qui suit.
 	 */
 	class ButtonListener implements ActionListener{
 		public void actionPerformed(ActionEvent arg0) 
 		{
 			System.out.println("Action Performed");
-		}
-	}
-	
-	class ButtonBarChartListener implements ActionListener{
-		public void actionPerformed(ActionEvent arg0) 
-		{
-			final BarChart bar = new BarChart("Poids des couleurs","Résultats");
-			bar.pack();
-			RefineryUtilities.centerFrameOnScreen(bar);
-	        bar.setVisible(true);
 		}
 	}
 }
